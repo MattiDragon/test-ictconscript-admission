6e44a2d19771075a2a356cb8d580e52883bec6bb
MattiDragon <matias.koivikko@gmail.com>
2026-07-17 21:31:13 +0300

    Initial work on backend

---
+++ backend/index.ts
@@ -0,0 +1,122 @@
+import express from "express";
+import sampleData from "./sample-data/data.json" with { type: "json" };
+import { DatabaseSync } from "node:sqlite";
+
+const db = new DatabaseSync("./db.sqlite");
+
+const addEntryStatement = db.prepare(
+  `INSERT INTO events (id, title, body, isoTime, lat, lon) VALUES (?, ?, ?, ?, ?, ?)`,
+);
+
+if (
+  !db
+    .prepare(
+      `SELECT name FROM sqlite_schema WHERE type='table' AND name='events'`,
+    )
+    .get()
+) {
+  db.exec(`CREATE TABLE events(
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    body TEXT NOT NULL,
+    isoTime TEXT NOT NULL,
+    lat REAL,
+    lon REAL
+    )`);
+  sampleData.forEach((dataPoint) => {
+    addEntryStatement.run(
+      dataPoint.id,
+      dataPoint.title,
+      dataPoint.body,
+      dataPoint.isoTime,
+      dataPoint.lat,
+      dataPoint.lon,
+    );
+  });
+}
+
+const prevId = (db.prepare(`SELECT max(id) FROM events`).get()!!["max(id)"] ||
+  0) as number;
+let nextId = prevId + 1;
+
+const app = express();
+
+app.get("/health", (req, res) => {
+  res.write("OK");
+});
+
+app.use(express.json());
+
+const listEntriesStatement = db.prepare(
+  `SELECT * FROM events ORDER BY isoTime DESC`,
+);
+app.get("/entries", (req, res) => {
+  const entries = listEntriesStatement.all();
+  res.json(entries);
+});
+
+const getEntryStatement = db.prepare(`SELECT * FROM events WHERE id = ?`);
+app.get("/entries/:id", (req, res) => {
+  let id: number;
+  try {
+    id = parseInt(req.params.id, 10);
+  } catch {
+    res.status(400).json({ error: "Invalid id" });
+    return;
+  }
+  res.json(getEntryStatement.get(id));
+});
+
+app.post("/entries", (req, res) => {
+  const payload = req.body;
+  if (typeof payload.title !== "string" || payload.title.length > 120) {
+    res
+      .status(400)
+      .json({ error: "Missing or invalid title, must be string <=120 chars" });
+    return;
+  }
+  if (typeof payload.body !== "string") {
+    res.status(400).json({ error: "Missing or invalid bod, must be string" });
+    return;
+  }
+
+  let lat = null;
+  let lon = null;
+  if (payload.lat !== undefined && payload.lon !== undefined) {
+    if (typeof payload.lat !== "number" || typeof payload.lon !== "number") {
+      res.status(400).json({
+        error:
+          "Invalid coordinates. If either is present, both lat and lon must be numbers",
+      });
+      return;
+    }
+    if (
+      payload.lat < -90 ||
+      payload.lat > 90 ||
+      payload.lon < -180 ||
+      payload.lon > 180
+    ) {
+      res.status(400).json({ error: "Coordinates out of range" });
+      return;
+    }
+    lat = payload.lat;
+    lon = payload.lon;
+  }
+
+  const id = nextId++;
+  addEntryStatement.run(
+    id,
+    payload.title,
+    payload.body,
+    new Date().toISOString(),
+    lat,
+    lon,
+  );
+
+  res.json(getEntryStatement.get(id));
+});
+
+const port = 8080;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
